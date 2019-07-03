b72b5c3a93d8383c79a8052aff474ec881117fbe
phuonganh <phuonganhbs1312@gmail.com>
2019-07-03 09:18:20 +0700

    Da sua server

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) =>{
-	fs.readFile('./views/joke.html', (err, data) => {
-		res.write(data);
-		return res.end();
-	});
-});
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('joke.ejs'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-server.listen(1312, 'localhost');
